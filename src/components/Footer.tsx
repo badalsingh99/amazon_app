8971d59cc68d9a4f8e6a09881c7795db584dc7e4
Badalsingh99 <badalsingh3233@gmail.com>
2023-08-01 22:57:18 +0530

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import logo from "../images/logo.png"
+
+const Footer = () => {
+  return (
+    <div className="w-full h-20 bg-amazon_light text-gray-300
+     flex items-center justify-center gap-4">
+        <Image className="w-24" src={logo} alt="logo" />
+        <p className="text-sm -mt-4">All Right Reserved <a className="hover:text-white hover:underline decoration-[1px] 
+        cursor-pointer duration-300" href="https://google.com" target="_blank">@Google</a></p>
+    </div>
+  )
+}
+
+export default Footer;
